6ea64845126a60e31b9d9c393623615b5e27fa54
kartegnik <99024925+kartegnik@users.noreply.github.com>
2024-09-07 14:26:56 +0500

    Update sparsematrix.h

--- sparsematrix.h
+++ sparsematrix.h
@@ -3,37 +3,111 @@
 #include <vector>
 #include<set>
 #include<map>
-
-using namespace std;
-const double eps = 1e-9;
+#include "sparseelement.h"
+//#include "tools.h"
 
 
 struct SparseMatrix {
 private:
 	int n;
 	int m;
-	vector<std::set<int>> spmi;
-	vector<std::set<int>> spmj;
-	map<pair<int, int>, double> spm;
+	std::vector<std::set<int>> spmi;
+	std::vector<std::set<int>> spmj;
+	std::map<std::pair<int, int>, double> spm;
 public:
+	/*
+		Конструктор матрицы по умолчанию
+	*/
+	explicit SparseMatrix();
+	/**/
 	explicit SparseMatrix(int n, int m); //просто задать размеры а потом заполнять
-	explicit SparseMatrix(vector<vector<double>> &a); //задачать плотную матрицу
-	explicit SparseMatrix(vector<double> a); //задачать диагональ
+	/*
+		Создать разреженную матрицу
+		n - длина
+		m - ширина
+		a - вектор из SparseElement
+	*/
+	explicit SparseMatrix(int n, int m, std::vector<SparseElement>& a);
+	/*
+		Создать разреденную матрицу из плотной
+	*/
+	explicit SparseMatrix(std::vector<std::vector<double>> &a); //задачать плотную матрицу
+	/*
+		Создать разреженную с диагональными элементами
+	*/
+	explicit SparseMatrix(std::vector<double> &a); //задачать диагональ
+	/*
+		Конструктор копирования
+	*/
 	SparseMatrix(SparseMatrix const& a);
+	/*
+		Оператор присваивания
+	*/
 	SparseMatrix& operator=(const SparseMatrix& other);
+	/*
+		Деструктор
+	*/
 	~SparseMatrix() {};
-
+	/*
+	Метод возвращает элемент на i и j позиции в матрице
+	*/
 	double get(int i, int j);
+	/*
+		Метод устанавливает в i и j позицию элемент v
+	*/
 	void set(int i, int j, double v);
-	vector<int> indexrow(int i);
-	vector<int> indexcolomn(int j);
+	/*
+		Метод добавляет в i и j позицию элемент v
+	*/
+	void addto(int i, int j, double v);
+	/*
+		Метод возвращает индексы столбцов элементов, которые есть в i строке
+	*/
+	std::set<int> indexrow(int i);
+	/*
+		Метод возварщает индексы строк элемнтов, которые есть в j стролбце
+	*/
+	std::set<int> indexcolumn(int j);
+	/*
+		Метод проверяет есть ли элемент в i и j позиции
+	*/
 	bool have(int i, int j);
-	pair<int, int> getnm();
-	void etmrow(int i, int j, double alpha); //сложить сткроки с коэффициентом у j строки, все добавляется в i строку
-	void etmcolomn(int i, int j, double alpha); // тоже но со столбцами
-	void print(); // какая-то странная получилась реализация
+	/*
+		Метод возвращает размерность матрицы
+		<длина, ширина>
+	*/
+	std::pair<int, int> getnm();
+	/*
+		Метод, прибавляющий к одной строке матрицы другую, умноженную на заданный скаляр
+		Аргументы:
+			x - индекс строки, к которой прибавляем
+			y - индекс строки, которую прибавляем
+			alpha - скаляр, на который умножается добавляемая строка
+	*/
+	void etomrow(int x, int y, double alpha); //сложить строки с коэффициентом у j строки, все добавляется в i строку
+	/*
+		Метод, прибавляющий к одному столбцу матрицы другой, умноженный на заданный скаляр 
+		Аргументы:
+			x - индекс столбца, к которому прибавляем
+			y - индекс столбца, который прибавляем
+			alpha - скаляр, на который умножается добавляемый столбец
+	*/
+	void etomcolumn(int x, int y, double alpha); // тоже но со столбцами
+	/*
+		Метод выводит всю матрицу
+	*/
+	void print();
+	/*
+		Метод проверяет i и j элемент находится в пределах матрицы
+	*/
 	bool in_matrix(int i, int j);
 private:
+	/*
+		Вспомогательный метод для оператора присваивания
+	*/
 	void swap(SparseMatrix& tmp);
+	/*
+		Метод удаляет элемент на i и j позиции
+	*/
 	void del(int i, int j);
 };
